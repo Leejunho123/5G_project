a1d8475eef8e696c97e8a888eb626bb0e922e7c2
Leejunho123 <n_aix@naver.com>
2021-03-08 12:14:53 +0900

    core

--- DeepLearning/dezero/core.py
+++ DeepLearning/dezero/core.py
@@ -44,38 +44,42 @@ class Variable:
     self.creator = func
     self.generation = func.generation + 1
   
-  def backward(self, retain_grad = False, create_graph=False):
-    if self.grad is None:
-      self.grad = Variable(np.ones_like(self.data))
+ def backward(self, retain_grad=False, create_graph=False):
+        if self.grad is None:
+            xp = dezero.cuda.get_array_module(self.data)
+            self.grad = Variable(xp.ones_like(self.data))
 
-    funcs = []
-    seen_set = set()
+        funcs = []
+        seen_set = set()
 
-    def add_func(f):
-      if f not in seen_set:
-        funcs.append(f)
-        seen_set.add(f)
-        funcs.sort(key = lambda x: x.generation)
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
 
-    add_func(self.creator)
-    while funcs:
-      f = funcs.pop()
-      gys = [output().grad for output in f.outputs]
-      
-      with using_config('enable_backprop', create_graph):
-        gxs = f.backward(*gys)
-        if not isinstance(gxs, tuple) :
-          gxs = (gxs,)
-        for x, gx in zip(f.inputs, gxs):
-          if x.grad is None:
-            x.grad = gx
-          else:
-            x.grad = x.grad + gx
-          if x.creator is not None:
-            add_func(x.creator)
-      if not retain_grad:
-        for y in f.outputs:
-          y().grad = None
+        add_func(self.creator)
+        while funcs:
+            f = funcs.pop()
+            gys = [output().grad for output in f.outputs]  # output is weakref
+
+            with using_config('enable_backprop', create_graph):
+                gxs = f.backward(*gys)
+                if not isinstance(gxs, tuple):
+                    gxs = (gxs,)
+
+                for x, gx in zip(f.inputs, gxs):
+                    if x.grad is None:
+                        x.grad = gx
+                    else:
+                        x.grad = x.grad + gx
+
+                    if x.creator is not None:
+                        add_func(x.creator)
+
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None  # y is weakref
   def cleargrad(self):
     self.grad = None
   @property
