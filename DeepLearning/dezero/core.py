56f578aaa645856bee2cdb85ab73f2095e75725b
Leejunho123 <n_aix@naver.com>
2021-03-08 15:55:37 +0900

    Add: Variable transpose

--- DeepLearning/dezero/core.py
+++ DeepLearning/dezero/core.py
@@ -85,6 +85,9 @@ class Variable:
     if len(shape) == 1 and isinstance(shape[0], (tuple, list)):
       shape = shape[0]
     return dezero.functions.reshape(self, shape)
+
+  def transpose(self):
+    return dezero.functions.transpose(self)
   def cleargrad(self):
     self.grad = None
   @property
