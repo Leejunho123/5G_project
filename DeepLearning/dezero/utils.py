7a5704d2113ac5d36c7eae39651013d2de19f6ca
Leejunho <73324807+Leejunho123@users.noreply.github.com>
2021-03-08 10:44:16 +0900

    Update utils.py

--- DeepLearning/dezero/utils.py
+++ DeepLearning/dezero/utils.py
@@ -7,10 +7,10 @@ from dezero import Variable
 
 def _dot_var(v, verbose=False):
   dot_var = '{} [label={}, color=orange, style=filled]\n'
-  name = '' if v.name is None else v.name
+  name = ' ' if v.name is None else v.name
   if verbose and v.data is not None :
     if v.name is not None:
-      name += ': '
+      name += ' '
     name += str(v.shape) + ' ' + str(v.dtype)
   return dot_var.format(id(v),name)
 
