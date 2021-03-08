243cafafe74ebd49c5db09e85c58cb0afd97dd43
Leejunho123 <n_aix@naver.com>
2021-03-08 15:48:36 +0900

    Add: Variable reshape

--- DeepLearning/dezero/core.py
+++ DeepLearning/dezero/core.py
@@ -79,6 +79,11 @@ class Variable:
             if not retain_grad:
                 for y in f.outputs:
                     y().grad = None  # y is weakref
+  
+  def reshape(self, *shape):
+    if len(shape) == 1 and isinstance(shape[0], (tuple, list)):
+      shape = shape[0]
+    return dezero.functions.reshape(self, shape)
   def cleargrad(self):
     self.grad = None
   @property
