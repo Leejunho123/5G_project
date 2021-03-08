489290b9cd52bb24373cb5fc58ffad3f035f5037
Leejunho <73324807+Leejunho123@users.noreply.github.com>
2021-03-08 11:52:52 +0900

    Update __init__.py

--- DeepLearning/dezero/__init__.py
+++ DeepLearning/dezero/__init__.py
@@ -9,7 +9,7 @@ if is_simple_core :
   from dezero.core_simple import setup_variable
 
 else :
-  from dezero.core import as_variable
+  from dezero.core import Variable
   from dezero.core import Function
   from dezero.core import using_config
   from dezero.core import no_grad
