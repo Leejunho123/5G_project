d0533354506e1dadfec2a14a8b6d5ddc5d5afe1e
Leejunho123 <n_aix@naver.com>
2021-03-09 17:12:00 +0900

    MLP

--- DeepLearning/dezero/models.py
+++ DeepLearning/dezero/models.py
@@ -15,10 +15,10 @@ class MLP(Model):
         self.activation = activation
         self.layers = []
 
-    for i, out_size in enumerate(fc_output_sizes):
-        layer = L.Linear(out_size)
-        setattr(self, 'l' + str(i), layer)
-        self.layers.append(layer)
+        for i, out_size in enumerate(fc_output_sizes):
+            layer = L.Linear(out_size)
+            setattr(self, 'l' + str(i), layer)
+            self.layers.append(layer)
 
     def forward(self, x):
         for l in self.layers[:-1]:
