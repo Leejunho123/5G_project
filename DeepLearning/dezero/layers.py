dbc3ff4cbf896671df10d2b82ac9038099c659c8
Leejunho123 <n_aix@naver.com>
2021-03-09 14:48:48 +0900

    Linear Layer

--- DeepLearning/dezero/layers.py
+++ DeepLearning/dezero/layers.py
@@ -34,7 +34,7 @@ class Layer:
 
 class Linear(Layer):
   def __init__(self, out_size, nobais = False, dtype = np.float32, in_size = None):
-    super().__init()
+    super().__init__()
     self.in_size = in_size
     self.out_size = out_size
     self.dtype = dtype
