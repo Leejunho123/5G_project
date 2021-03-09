9fb9d69b52bfeeeab5d7746de46f838fcda2d193
Leejunho123 <n_aix@naver.com>
2021-03-09 12:03:42 +0900

    Fix: Sigmoid

--- DeepLearning/dezero/functions.py
+++ DeepLearning/dezero/functions.py
@@ -221,7 +221,8 @@ def sigmoid_simple(x):
 
 class Sigmoid(Function):
   def forward(self, x):
-    y = 1 / (1 + np.exp(-x))
+    x = -1*x
+    y = 1 / (1 + np.exp(x))
     return y
 
   def backward(self, gy):
