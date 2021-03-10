d2ba143cdbaf1db9c9c4155cf0f0f2ab2d0e6a54
Leejunho123 <n_aix@naver.com>
2021-03-10 10:33:51 +0900

    DataLoader

--- DeepLearning/dezero/datasets.py
+++ DeepLearning/dezero/datasets.py
@@ -1,4 +1,6 @@
 import numpy as np
+import math
+import random
 
 class Dataset:
   def __init__(self, train=True, transform=None, target_transform=None):
@@ -70,3 +72,44 @@ class BigData(Dataset):
   def __len__():
     return 1000000
 
+
+
+
+class DataLoader:
+  def __init__(self, dataset, batch_size, shuffle = True):
+    self.dataset = dataset
+    self.batch_size = batch_size
+    self.shuffle = shuffle
+    self.data_size = len(dataset)
+    self.max_iter = math.ceil(self.data_size / batch_size)
+
+    self.reset()
+
+  def resert(self):
+    self.iteration = 0
+    if self.shuffle:
+      self.index = np.random.permutation(len(self.dataset))
+
+    else:
+      self.index = np.arange(len(self.dataset))
+
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    if self.iteration >= self.max_iter:
+      self.reset()
+
+      raise StopIteration
+
+    i , batch_size = self.iteration, self.batch_size
+    batch_index = self.index[i * batch_size:(i+1)*batch_size]
+    batch = [self.dataset[i] for i in batch_index]
+    x = np.array([example[0] for example in batch])
+    t = np.array([example[1] for example in batch])
+
+    self.iteration += 1
+    return x, t
+
+  def next(self):
+    return self.__next__()
